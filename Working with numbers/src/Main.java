a7a6db8dadf94e93709f7b61e670777ca935a503
PardEd <edbashirov@gmail.com>
2022-03-05 02:01:04 +0500

    project Working with numbers

---
+++ Working with numbers/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> result = new ArrayList<>();
+
+        for (Integer x : intList) {
+            if (x > 0) {
+                if (x % 2 == 0) {
+                    result.add(x);
+                }
+            }
+        }
+        result.sort(Comparator.naturalOrder());
+        for (Integer x : result) {
+            System.out.println(x);
+        }
+    }
+}
